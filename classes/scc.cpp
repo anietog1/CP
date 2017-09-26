2955168f503a6f83b3e7792963dc9c8852671061
Agustin Nieto Garcia <anietog1@eafit.edu.co>
2017-09-25 20:35:47 -0500

    Anadiendo algoritmo de kusaraju. MAS CUIDADO ¬-¬ tardaste 3h debuggeando

--- classes/scc.cpp
+++ classes/scc.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+//Kusaraju's algorithm with 1-index
 using namespace std;
 
-#define MAXN 50
-#define D(x) cout << #x << " ) " << x << endl
+#define MAXN 100
+#define D(x) cout << #x << " =  " << x << endl
 
 vector<int> sortT;
 vector<int> g[MAXN];
@@ -12,24 +13,58 @@ vector<int> rg[MAXN];
 int scc[MAXN];
 bool state[MAXN];
 
-void sortT(int n){
-  state[n] = true;
+int n;
 
-  for(int i=0; i<=n; ++i){
-    int next = g[n][i];
-    if(!state[next]) sortT(next);
+void sortTop(int s){
+  state[s] = true;
+
+  for(int i=0; i<g[s].size(); ++i){
+    int next = g[s][i];
+    if(!state[next]) sortTop(next);
   }
 
-  sortT.push_back(n);
+  sortT.push_back(s);
 }
 
-void dfs(int n, int cIdx){
+void dfs(int s, int cIdx){//Armando las scc
+  scc[s] = cIdx;
   
+  for(int i=0; i < rg[s].size(); ++i){
+    int next = rg[s][i];
+    if(scc[next] == -1) dfs(next, cIdx);
+  }
+}
+
+void findSCC(){
+  for(int i=1; i<=n; ++i)
+    if(!state[i]) sortTop(i);
+  reverse(sortT.begin(), sortT.end());
+
+  int cIdx = 1;
+  for(int i=0; i<sortT.size(); ++i){
+    int next = sortT[i];
+    if(scc[next] == -1){
+      dfs(next, cIdx);
+      ++cIdx;
+    }
+  }
+}
+
+void reset(){
+  sortT.clear();
+
+  for(int i=1; i<=n; ++i){
+    state[i] = false;
+    g[i].clear();
+    rg[i].clear();
+    scc[i] = -1;
+  }
 }
 
 int main(){
-  int n, m, a, b;
+  int m, a, b;
   cin >> n >> m;
+  reset();
   
   while(m){
     cin >> a >> b;
@@ -37,12 +72,9 @@ int main(){
     rg[b].push_back(a);
     --m;
   }
+
+  findSCC();
   
-  for(int i=0; i<=n; ++i)
-    scc[i] = -1;
-  
-  findSCC(n);
-  
-  for(int i=0; i<=n; ++i)
+  for(int i=1; i<=n; ++i)
     cout << i << " = " << scc[i] << endl;
 }
