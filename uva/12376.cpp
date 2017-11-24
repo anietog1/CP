a92fce951b2fa464f4700164ee6ecc8dc2f62329
U-CINFO\anietog1 <anietog1@PTSAL56526.cinfo.eafit.edu.co>
2017-11-24 16:32:24 -0500

    agregando 1 mas. falla, corregir.

---
+++ uva/12376.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+#define MAXN 110
+
+int T, n, m, u, v, point[MAXN];
+vector<int> g[MAXN];
+int endNode = 0;
+
+int max(int curr){
+  if(g[curr].size() == 0){
+    endNode = curr;
+    return 0;
+  }
+  
+  int maxPoints = 0;
+  int maxNode;
+  
+  for(int i=0;i<g[curr].size();++i){
+    int next = g[curr][i];
+
+    if(point[next] > maxPoints){
+      maxPoints = point[next];
+      maxNode = next;
+    }
+  }
+
+  return maxPoints + max(maxNode);
+}
+
+int main(){
+  cin >> T;
+  for(int i=1;i<=T; ++i){
+    cin >> n >> m;
+    
+    for(int i=0;i<n;++i){
+      cin >> point[i];
+      g[i].clear();
+    }
+    
+    while(m--){
+      cin >> u >> v;
+      g[u].push_back(v);
+    }
+
+    cout << "Case " << i << ": " << max(0) << " " << endNode << endl;
+  }
+}
