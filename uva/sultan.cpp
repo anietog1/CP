fe95bb54ef10ce839d29bad3334fb126b9f1b8fd
Agustin <anietog1@eafit.edu.co>
2017-11-28 12:39:01 -0500

    Los 3 de hoy :)

---
+++ uva/sultan.cpp
@@ -0,0 +1,51 @@
+//https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=666&page=show_problem&problem=4027
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+#define MAXN 30
+#define d(x) cout << #x << " = " << x << endl;
+
+int t, idx;
+string walk;
+vector<int> g[MAXN];
+
+void clear(){
+  idx = 0;
+  for(int i=0;i<MAXN;++i)
+    g[i].clear();
+}
+
+int next(){
+  return walk[idx++] - 'A';
+}
+
+void traverse(int curr){
+  int nextnode;
+
+  while((nextnode = next()) != curr){
+    //d(nextnode);
+    g[nextnode].push_back(curr);
+    g[curr].push_back(nextnode);
+    traverse(nextnode);
+  }
+}
+
+int main(){
+  cin >> t;
+  for(int turn = 1; turn <= t; ++turn){
+    cin >> walk;
+    clear();
+    traverse(next());
+
+    cout << "Case " << turn << endl;
+    
+    for(int i=0;i<MAXN;++i){
+      int size = g[i].size();
+      
+      if(size)
+	cout << (char)(i + 'A') << " = " << size << endl;
+    }
+  }
+}
