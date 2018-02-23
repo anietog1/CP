cad19f15b092412bd188ede39ab6cad643d8178c
Agustín Nieto <anietog1@eafit.edu.co>
2018-02-23 08:39:03 -0500

    Submitting last solution to graph colouring problem

--- classes/stoplights.cpp
+++ classes/stoplights.cpp
@@ -2,48 +2,51 @@
 #include <vector>
 
 using namespace std;
-
+//Complexity: O(n**3)
+//Without noticing, the solution is the same presented in the book
+/*
+ * The other solution was:
+ * for each node in g
+ * |for each color in g
+ * |||color node with color
+ * |||if adjacent to a node with same color
+ * |||color with next color
+ * ||repeat
+ * 
+ * The complexity of this one was O(n**3) too, but is way more
+ * difficult to implement.
+ */
 int main(){
   int n, m;
   cin >> n >> m;
-  int matrix[n+1][n+1] = {};//gonna work 1 index
-  while(m--){//build matrix
+  int matrix[n+1][n+1] = {};
+  while(m--){
     int u, v;
     cin >> u >> v;
     matrix[u][v] = matrix[v][u] = true;
   }
-  bool colored[n+1] = {};//init colored
-  vector<vector<int>> groups;//init groups
-  for(int i=1;i<=n;++i){
+  bool colored[n+1] = {};
+  vector<vector<int>> groups;
+  for(int i=1;i<=n;++i)
     if(!colored[i]){
-      bool forbidden[n+1] = {};//init forbidden
-      for(int j=1;j<=n;++j){
-	if(matrix[i][j]){//forbid connected ones
-	  forbidden[j] = true;
-	}
-      }
-      for(int j=1;j<=n;++j){
-	if(!colored[j]/*added this param, for making less groups (avoiding take always smaller nums)*/ && !forbidden[j]){//if it will contain j, then forbid forbidden ones for j
-	  for(int k=1;k<=n;++k){
-	    if(matrix[j][k]){//forbid forbidden ones for j
-	      forbidden[k] = true;
-	    }
-	  }
-	}
-      }
+      bool forbidden[n+1] = {};
       
-      vector<int> currgroup;//make group
-      for(int j=1;j<=n;++j){
-	if(!forbidden[j]){//if can ally with j, then do it
+      for(int j=i;j<=n;++j)
+	if(!colored[j] && !forbidden[j])
+	  for(int k=1;k<=n;++k)
+	    if(matrix[j][k])
+	      forbidden[k] = true;
+      
+      vector<int> currgroup;
+      for(int j=1;j<=n;++j)
+	if(!forbidden[j]){
 	  colored[j] = true;
 	  currgroup.push_back(j);
 	}
-      }
-      groups.push_back(currgroup);//add the new group
+      groups.push_back(currgroup);
     }
-  }
   
-  for(int i=0;i<groups.size();++i){//print groups
+  for(int i=0;i<groups.size();++i){
     cout << "Group #" << i << ":" << endl;
     for(int j=0;j<groups[i].size();++j){
       if(j){
@@ -54,7 +57,3 @@ int main(){
     cout << endl;
   }
 }
-
-//Notes:
-//First solution did always choose smaller numbers, then leaving a lot of groups
-//Second solution (current one) is, without I even noticed, the same as the one in the book...
