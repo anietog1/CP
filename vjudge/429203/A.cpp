fad36ca3ab8b549030a343f900adb56dbf61365e
Agustín Nieto García <agustin.nieto@talosdigital.com>
2021-03-23 11:31:34 -0500

    Pushed the wrong one

--- vjudge/429203/A.cpp
+++ vjudge/429203/A.cpp
@@ -6,35 +6,41 @@ using namespace std;
 #define pii pair<int, int>
 
 int main() {
-  int m, n; cin >> n >> m;
-  vector<pii> g[m];
-  while(n--) {
-    int x, y, z; cin >> x >> y >> z;
-    g[x].push_back(pii(z, y));
-    g[y].push_back(pii(z, x));
-  }
+  int m, n;
+  
+  while (cin >> m >> n, m | n) {
+    vector<pii> g[m];
+    int total = 0;
+    while(n--) {
+      int x, y, z; cin >> x >> y >> z;
+      g[x].push_back(pii(z, y));
+      g[y].push_back(pii(z, x));
+      total += z;
+    }
 
-  bool visited[m] = {};
-  priority_queue<pii, vector<pii>, greater<pii>> q;
-  int sum = 0;
-  q.push(pii(0, 0));
-  while(!q.empty()) {
-    int curr = q.top().second;
-    int add = q.top().first;
-    q.pop();
-    if (visited[curr]) continue;
-    sum += add;
-    visited[curr] = true;
-    
-    for(int i = 0; i < g[curr].size(); i++) {
-      int next = g[curr][i].second;
-      int nextsum = g[curr][i].first;
+    bool visited[m] = {};
+    priority_queue<pii, vector<pii>, greater<pii>> q;
 
-      if (!visited[next]) {
-        q.push(pii(nextsum, next));
+    int sum = 0;
+    q.push(pii(0, 0));
+    while(!q.empty()) {
+      int curr = q.top().second;
+      int add = q.top().first;
+      q.pop();
+      if (visited[curr]) continue;
+      sum += add;
+      visited[curr] = true;
+
+      for(int i = 0; i < g[curr].size(); i++) {
+        int next = g[curr][i].second;
+        int nextsum = g[curr][i].first;
+
+        if (!visited[next]) {
+          q.push(pii(nextsum, next));
+        }
       }
     }
-  }
 
-  cout << sum << endl;
+    cout << total - sum << endl;
+  }
 }
